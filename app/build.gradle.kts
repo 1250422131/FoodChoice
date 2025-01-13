18de8536cd5829fd7bdb0ddb73001ddb98c7fef0
萌新杰少 <1250422131@qq.com>
2025-01-13 17:34:53 +0800

    1.0.0版本发布

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.imcys.foodchoice"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 100
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
